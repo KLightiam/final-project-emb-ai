f0926f19ad317ce697a0cbe5a0c1fdf2ba18c543
KLightiam <kingsleyasare20@yahoo.com>
2026-01-16 06:44:37 -0500

    add emotion score to emotion detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,7 +8,21 @@ def emotion_detector (text_to_analyze):
 
     response = requests.post(url, json = input_json, headers = headers)
 
-    # response_json = json.loads(response.text)
+    response_json = json.loads(response.text)
+    pick_emotion = response_json['emotionPredictions'][0]['emotion']
+    anger_score = pick_emotion['anger']
+    disgust_score = pick_emotion['disgust']
+    fear_score = pick_emotion['fear']
+    joy_score = pick_emotion['joy']
+    sadness_score = pick_emotion['sadness']
+    dominant_emotion = ''
+    counter = 0
+
+    for emotion,emotion_value in enumerate(pick_emotion):
+        if float(emotion_value) > counter:
+            counter = float(emotion_value)
+            dominant_emotion = emotion
+
 
     return response.text
 
