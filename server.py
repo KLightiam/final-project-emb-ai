356df9dc11380708473234c1414e9e2462621317
KLightiam <kingsleyasare20@yahoo.com>
2026-01-16 11:00:32 -0500

    add static code analysis

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
+''' Executing this function initiates the emotion detection
+    application using flask
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def emotion_analyzer():
+    '''This function runs analysis on the input at /emotionDetector
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
-
-    # response_str = ''
-    # for key,value in response.items():
-    #     response_str += f"{key}:{value}" 
 
     final_result = f"For the given statement, the system response is 'anger':{response['anger']}, \
     'disgust':{response['disgust']}, 'fear':{response['fear']}, \
@@ -19,12 +21,14 @@ def emotion_analyzer():
 
     if not response['dominant_emotion']:
         return "Invalid text! Please try again!"
-    else:
-        return final_result
+
+    return final_result
 
 
 @app.route('/')
 def render_home():
+    '''This method runs the homepage template
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
